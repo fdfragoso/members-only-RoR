45293919c419776592329c9ec74ed7a002bca380
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-17 18:06:39 +0300

    Add user reference to the post migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_142459) do
+ActiveRecord::Schema.define(version: 2020_09_17_145829) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
