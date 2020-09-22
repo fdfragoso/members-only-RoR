5b62aa60dff7359acca049b36623fc1ea139d434
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-22 18:17:47 +0300

    Add user and post validation, association and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_145223) do
+ActiveRecord::Schema.define(version: 2020_09_22_150921) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "body"
-  end
+# Could not dump table "posts" because of following StandardError
+#   Unknown type '' for column 'user'
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -27,4 +26,5 @@ ActiveRecord::Schema.define(version: 2020_09_22_145223) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
