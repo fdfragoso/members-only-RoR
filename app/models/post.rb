2fd0794911ca997d9cfc12279461a4dbbb8d3aeb
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-25 10:56:55 +0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    validates :body, presence: true
-    validates :user_id, presence: true
+  belongs_to :user
+  validates :body, presence: true
+  validates :user_id, presence: true
 end
